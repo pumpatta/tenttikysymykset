483b4d04a911195cf42a819a74f0a80e5fec8e95
taappana <herranen.jani@gmail.com>
2018-02-22 10:55:44 +0200

    Lisätty package ja yhteysluokka

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -1,3 +1,5 @@
+package fi.academy;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/fi/academy/Yhteys.java
@@ -0,0 +1,16 @@
+package fi.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Yhteys {
+    public static Connection avaaYhteys() throws SQLException, ClassNotFoundException {
+        String connstr = String.format("jdbc:mysql://localhost:3306/tentti?useSSL=false");
+        tarkistaAjuriluokka();
+        return DriverManager.getConnection(connstr, "root" ,"salanana");
+    }
+    private static void tarkistaAjuriluokka() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+}
